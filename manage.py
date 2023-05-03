3ca9cc4ba44825f3b3cd420e0f011db437f4a958
elianys-mar <elianysm@buffalo.edu>
2023-05-03 14:30:00 -0400

    week12

--- manage.py
+++ manage.py
@@ -1,17 +1,25 @@
-import sqlite3
+import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, template_folder='template')
+Bootstrap(app)
 app.secret_key = "secret key"
 
-conn = sqlite3.connect('employees.db')
+conn = mysql.connector.connect(
+	host='localhost',
+	user='flask',
+	password='Michelle@26',
+	database= 'week12'
+)
+
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS employees (
-        EmpID INTEGER PRIMARY KEY AUTOINCREMENT,
-        EmpName TEXT,
-        EmpGender TEXT,
-        EmpPhone TEXT,
-        EmpBdate DATE)''')
+        EmpID INT AUTO_INCREMENT PRIMARY KEY,
+        EmpName VARCHAR(200),
+        EmpGender VARCHAR(200),
+        EmpPhone VARCHAR(200),
+        EmpBdate VARCHAR(200))''')
 print("Table created")
 conn.close()
 
@@ -27,9 +35,14 @@ def empregistration():
         empphone = request.form['EmpPhone']
         empbdate = request.form['EmpBdate']
 
-        conn = sqlite3.connect('employees.db')
+        conn = mysql.connector.connect(
+		host='localhost',
+		user='flask',
+		password='Michelle@26',
+		database= 'week12'
+)
         c = conn.cursor()
-        c.execute("INSERT INTO employees (EmpName, EmpGender, EmpPhone, EmpBdate) VALUES (?, ?, ?, ?)" , (empname, empgender, empphone, empbdate))
+        c.execute("INSERT INTO employees (EmpName, EmpGender, EmpPhone, EmpBdate) VALUES ('{0}','{1}','{2}','{3}')" , (empname, empgender, empphone, empbdate))
         conn.commit()
         conn.close()
 
@@ -39,11 +52,16 @@ def empregistration():
 
 @app.route('/empinformation')
 def empinformation():
-    with sqlite3.connect('employees.db') as con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM employees")
-        rows = cur.fetchall()
-    return render_template('empinformation.html', rows=rows)
+	conn = mysql.connector.connect(
+		host='localhost',
+		user='flask',
+		password='Michelle@26',
+		database= 'week12'
+)
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM employees")
+	rows = cur.fetchall()
+	return render_template('empinformation.html', rows=rows)
 
 if __name__ == '__main__':
     app.run(debug=True)
